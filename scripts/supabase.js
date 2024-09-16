7dcd7d24987dadbb0b92872ff294e63ddf0bb743
Afonso <afonsojansen@gmail.com>
2024-09-16 14:23:29 -0300

    usuarios

--- scripts/supabase.js
+++ scripts/supabase.js
@@ -1,8 +1,8 @@
 // scripts/supabase.js
 
-// Definir a URL e a chave pública do Supabase
+// Definir a URL e a chave pública (anon key) do Supabase
 const SUPABASE_URL = 'https://kiedafitbtuednyftkuv.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...';  // Substitua pela sua chave
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtpZWRhZml0YnR1ZWRueWZ0a3V2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY0OTY4MDIsImV4cCI6MjA0MjA3MjgwMn0.qdSfW6CfRJ3XVv7UZ_MVW9lWJQrk4TA8pgWRi0BqKKc';
 
 // Criar cliente Supabase usando o objeto global
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
