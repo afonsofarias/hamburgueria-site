ebeb9e91f22284e47af3cae3e83b6242c805f35f
Afonso <afonsojansen@gmail.com>
2024-09-16 12:57:39 -0300

    Corrige a inicialização do supabase

--- scripts/supabase.js
+++ scripts/supabase.js
@@ -1,6 +1,6 @@
 // scripts/supabase.js
 
-// Atualize com a sua URL e chave do Supabase
+// Definindo a URL e a chave do Supabase
 const SUPABASE_URL = 'https://kiedafitbtuednyftkuv.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtpZWRhZml0YnR1ZWRueWZ0a3V2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY0OTY4MDIsImV4cCI6MjA0MjA3MjgwMn0.qdSfW6CfRJ3XVv7UZ_MVW9lWJQrk4TA8pgWRi0BqKKc';
 
