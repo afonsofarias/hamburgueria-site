945303b9b5aa1d7ac463290f60fafb50ffab1f6f
Afonso <afonsojansen@gmail.com>
2024-09-16 21:33:37 -0300

    usuarios

--- scripts/supabase.js
+++ scripts/supabase.js
@@ -1,8 +1,5 @@
-import { createClient } from '@supabase/supabase-js';
-
-// Dados de acesso ao Supabase
+// Inicializar o cliente do Supabase no navegador
 const SUPABASE_URL = 'https://kiedafitbtuednyftkuv.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtpZWRhZml0YnR1ZWRueWZ0a3V2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY0OTY4MDIsImV4cCI6MjA0MjA3MjgwMn0.qdSfW6CfRJ3XVv7UZ_MVW9lWJQrk4TA8pgWRi0BqKKc';
 
-// Inicializar o cliente do Supabase
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
